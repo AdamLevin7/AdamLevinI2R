6c0a9e3b4cbce57e6830f858a54b9c6ed8e0a217
Slaith <34562469+Slaith12@users.noreply.github.com>
2022-08-14 16:06:23 -0700

    Revert "turret progress but cant see bullets"
    This reverts commit b6cfe242a0419193a8906b82ac08dfa80fab226b.

--- Wowei/Assets/Scripts/TurretController.cs
+++ Wowei/Assets/Scripts/TurretController.cs
@@ -4,26 +4,6 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour
 {
-    public float range;
-
-    public Transform target;
-
-    bool detected = false;
-
-    Vector2 Direction;
-
-    public GameObject gun;
-
-    public GameObject Bullet;
-
-    public float FireRate;
-
-    float nextTimeToFire = 0;
-
-    public Transform ShootPoint;
-
-    public float Force;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -33,44 +13,6 @@ public class TurretController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetPos = target.position;
-        Direction = targetPos - (Vector2)transform.position;
-        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, range);
-        if (rayInfo)
-        {
-            if(rayInfo.collider.gameObject.tag == "Player")
-            {
-                if(detected == false)
-                {
-                    detected = true;
-                    
-                }
-            }
-            else
-            {
-                if(detected == true)
-                {
-                    detected = false;
-                }
-            }
-        }
-        if (detected)
-        {
-            gun.transform.up = Direction;
-            if(Time.time > nextTimeToFire)
-            {
-                nextTimeToFire = Time.time + 1 / FireRate;
-                shoot();
-            }
-        }
-    }
-    void shoot()
-    {
-        GameObject BulletIns = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
-        BulletIns.GetComponent<Rigidbody2D>().AddForce(Direction * Force);
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.DrawWireSphere(transform.position, range);
+        
     }
 }
